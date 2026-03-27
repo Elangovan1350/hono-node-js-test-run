2735c666436b36958140d09faf5759b0de27418c
Elangovan1350 <elangovan2019miss@gmail.com>
2026-03-27 08:13:08 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/hello/:name", (c) => {
+  const { name } = c.req.param();
+  return c.text(`Hello ${name}!`);
+});
+app.post("/login", async (c) => {
+  const { name, email, password } = await c.req.json();
+  return c.json({ name, email, password });
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3001,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
